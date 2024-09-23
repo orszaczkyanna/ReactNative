3a82c6f58578ef38a8f4f1d2685fa4ff870f7f5e
orszaczkyanna <orszaczky.anna@gmail.com>
2024-09-23 15:17:03 +0200

    Meditation Index Screen (Meditation)

--- meditation-app-StevenCodeCraft/app/(tabs)/nature-meditate.tsx
+++ meditation-app-StevenCodeCraft/app/(tabs)/nature-meditate.tsx
@@ -1,9 +1,54 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Pressable, ImageBackground } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+
+import AppGradient from "@/components/AppGradient";
+import { MEDITATION_DATA } from "@/constants/MeditationData"; // export
+import MEDITATION_IMAGES from "@/constants/meditation-images"; // export default
 
 const NatureMeditate = () => {
   return (
     <View className="flex-1">
-      <Text>Welcome</Text>
+      <AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
+        <View className="mb-6">
+          <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
+            Welcome
+          </Text>
+          <Text className="text-indigo-100 text-xl font-medium">
+            Start your meditation practice today
+          </Text>
+        </View>
+        <View>
+          <FlatList
+            data={MEDITATION_DATA}
+            keyExtractor={(item) => item.id.toString()}
+            className="mb-20"
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() => console.log(item.title)}
+                className="h-48 my-3 rounded-md overflow-hidden" // overflow-hidden nélkül nem kerekíti le
+              >
+                <ImageBackground
+                  source={MEDITATION_IMAGES[item.id - 1]} // az id 1-től indul
+                  resizeMode="cover"
+                  className="flex-1 rounded-lg justify-center"
+                >
+                  <LinearGradient
+                    colors={["transparent", "rgba(0, 0, 0, 0.8)"]} // !transparent
+                    className="flex-1 justify-center items-center"
+                  >
+                    <Text className="text-gray-100 text-3xl font-bold text-center">
+                      {item.title}
+                    </Text>
+                  </LinearGradient>
+                </ImageBackground>
+              </Pressable>
+            )}
+          />
+        </View>
+      </AppGradient>
+      <StatusBar style="light" />
     </View>
   );
 };
